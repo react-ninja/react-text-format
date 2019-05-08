036600e68956b423f8926daa1f0540225f1e994a
ahsanbilalas <ahsan.bilal@arbisoft.com>
2019-05-08 19:54:40 +0500

    Adding docs

---
+++ docs/precache-manifest.d7d7ec3d631e41ac72b56fa5a52e3733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b614af6102b1f5bca5d0b7526cf7b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1b2cc77f4aa10da16a7",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "dc635c1c81eb89d377e1",
+    "url": "/static/js/2.9e255dc3.chunk.js"
+  },
+  {
+    "revision": "c1b2cc77f4aa10da16a7",
+    "url": "/static/js/main.a26b3935.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
