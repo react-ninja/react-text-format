f56b934d618daede29568a113821061fe2ddddae
ahsanbilalas <ahsan.bilal@arbisoft.com>
2019-05-09 22:01:31 +0500

    Adding build to demo

---
+++ demo/precache-manifest.24114ce8c1dec631308afeb43275d073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88de978679710c6425f4c159f6bb85a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71413c6ee87a832d3dd4",
+    "url": "/static/css/2.08c8ee57.chunk.css"
+  },
+  {
+    "revision": "a8a1afd8223fefff7000",
+    "url": "/static/css/main.b9f29c93.chunk.css"
+  },
+  {
+    "revision": "71413c6ee87a832d3dd4",
+    "url": "/static/js/2.e1e984a0.chunk.js"
+  },
+  {
+    "revision": "a8a1afd8223fefff7000",
+    "url": "/static/js/main.dbb7ccae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
